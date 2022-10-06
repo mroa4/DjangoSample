16628e96992a3187daba83c9488ee9f997173f93
mroa4 <omidamini360@gmail.com>
2022-10-06 13:59:10 -0300

    step 5/6 - add trip to model and form

--- ports/forms.py
+++ ports/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from ports.models import Port
+from ports.models import Port, Trip
 
 
 class PortForm(ModelForm):
     class Meta:
         model = Port
         fields = ['name', 'temp', 'latitude', 'longitude']
+
+class TripForm(ModelForm):
+    class Meta:
+        model = Trip
+        fields = ['name', 'imo', 'trip_date', 'origin_port','dest_port']
--- ports/models.py
+++ ports/models.py
@@ -14,3 +14,11 @@ class Port(models.Model):
     longitude = models.FloatField(null=False, blank=False, validators=[
                                   MinValueValidator(-90), MaxValueValidator(90)])
     date = models.DateField(auto_now=True)
+
+class Trip(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50)
+    imo = models.IntegerField(null=False, blank=False, unique=True)
+    trip_date = models.DateField(null=False, blank=False)
+    submit_date = models.DateField(auto_now=True)
+    origin_port =  models.ForeignKey(Port, on_delete=models.PROTECT, related_name='origin')
+    dest_port = models.ForeignKey(Port, on_delete=models.PROTECT , related_name='destination')
